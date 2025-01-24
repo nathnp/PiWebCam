65e4cefe462fac7926cb6ed6306d7adedd795a62
Nathan Palmer <npalmer@intel-killer.local>
2025-01-23 19:13:51 -0500

    Added offline logic

--- cam.sh
+++ cam.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-user="<username>"
-server="<domain>"
-path="<site path>"
+user="nthp"
+server="nthp.me"
+path="/sites/cam/"
 
 if ! lsusb | grep -q "Webcam"; then
+	scp offline.jpg $user\@$server\:$path\cam.jpg
 	exit
 fi
+
+cd /home/pi/
 
 fswebcam -r "1280x720" --no-banner cam.jpg
 
